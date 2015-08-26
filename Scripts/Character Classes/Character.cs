3afe1fb02b2807ae73c2dac506ada84ea2502dbe
mattyiou <matthew_palma37@live.ca>
2015-08-26 18:39:31 -0400

    Basic Scripts

---
+++ Scripts/Character Classes/Character.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+    private int hp;
+    private bool isAlive;
+    private string name;
+
+    public void IncrementHP(int amount)
+    {
+        hp += amount;
+        if (hp <= 0)
+            isAlive = false;
+    }
+    public void DecrementHP(int amount)
+    {
+        hp -= amount;
+        if (hp <= 0)
+            isAlive = false;
+    }
+
+}
---
+++ Scripts/Player Controller/FlyingMovement.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using UnityEngine;
+
+public class FlyingMovement : GroundedMovement
+{
+    protected void Awake()
+    {
+        // Setting up references.
+        m_GroundCheck = transform.Find("GroundCheck");
+        m_CeilingCheck = transform.Find("CeilingCheck");
+        m_Anim = GetComponent<Animator>();
+        m_Rigidbody2D = GetComponent<Rigidbody2D>();
+        m_Rigidbody2D.mass = 0;
+    }
+
+    public void Move(float moveh, float movev, bool crouch, bool jump)
+    {
+        // If crouching, check to see if the character can stand up
+        if (!crouch && m_Anim.GetBool("Crouch"))
+        {
+            // If the character has a ceiling preventing them from standing up, keep them crouching
+            if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+            {
+                crouch = true;
+            }
+        }
+
+        // Set whether or not the character is crouching in the animator
+        m_Anim.SetBool("Crouch", crouch);
+
+        //only control the player if grounded or airControl is turned on
+        if (m_Grounded || m_AirControl)
+        {
+            // Reduce the speed if crouching by the crouchSpeed multiplier
+            moveh = (crouch ? moveh * m_CrouchSpeed : moveh);
+
+            // The Speed animator parameter is set to the absolute value of the horizontal input.
+            m_Anim.SetFloat("Speed", Mathf.Abs(moveh));
+
+            // The Speed animator parameter is set to the absolute value of the vertical input.
+            m_Anim.SetFloat("Speed", Mathf.Abs(movev));
+
+            // Move the character
+            m_Rigidbody2D.velocity = new Vector2(moveh * m_MaxSpeed, movev * m_MaxSpeed);
+
+            // If the input is moving the player right and the player is facing left...
+            if (moveh > 0 && !m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+            // Otherwise if the input is moving the player left and the player is facing right...
+            else if (moveh < 0 && m_FacingRight)
+            {
+                // ... flip the player.
+                Flip();
+            }
+        }
+        // If the player should jump...
+        if (m_Grounded && jump && m_Anim.GetBool("Ground"))
+        {
+            // Add a vertical force to the player.
+            m_Grounded = false;
+            m_Anim.SetBool("Ground", false);
+            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+        }
+    }
+}
